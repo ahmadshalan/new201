828a33715702441df092dd99c68f19a6bdc39752
ahmad shalan <engashalan@gmail.com>
2021-02-08 15:09:35 +0200

    new201

---
+++ app/app.js
@@ -0,0 +1,86 @@
+
+
+
+let name= prompt('what is your name?');
+
+
+alert('welcome'+' [' + name +']!'+' ' +'let us play a game');
+
+let myName= prompt('is my name Ahmad?').toLowerCase();
+
+
+if (myName=='yes'|| myName =='y'){
+
+    alert('you are right')
+}else{
+
+    alert('you are wrong, my name is ahmad')
+}
+
+console.log(myName);
+
+
+let age = prompt('am I 35?');
+
+if (age == 'yes' || age =='y'){
+
+    alert('you are wrong I am 32')
+
+
+}else if(age == 'no' || age =='n'){ 
+    alert ('you are right')
+
+}else{
+
+    alert('please answer yes or no')
+}
+console.log(age);
+
+let myProffesion = prompt ('Am I a Electrical Engineer?');
+
+if (myProffesion == 'yes' || myProffession == 'y'){
+
+    alert(' you are right')
+
+}else{
+
+    alert('you are wrong, I am an electrical engineer')
+}
+
+console.log(myProffesion);
+
+let children = prompt ('how many child do I have?');
+
+if (children == 1){
+     alert('you are right')
+
+}else{
+    alert('you are wrong, I have one')
+}
+
+console.log(children);
+
+let university = prompt ('Did I Study at J.U.S.T university?')
+
+if (university == 'yes' || universit =='y'){
+
+    alert('you are right')
+
+}else{
+    alert('you are wrong, I did')
+
+}
+
+console.log(university);
+
+
+
+
+
+
+
+
+
+
+
+
